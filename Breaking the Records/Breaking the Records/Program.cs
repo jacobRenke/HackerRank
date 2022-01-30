d6cc2543b17e4843a8a176eda5f2d811633c6bc1
Jacob Renke <jacob.m.renke@outlook.com>
2022-01-30 00:53:52 -0500

    complete

---
+++ Breaking the Records/Breaking the Records/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Breaking_the_Records
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+
+        public static List<int> breakingRecords(List<int> scores) // scores = [12, 24, 10, 24]
+        {
+            int maxScore = scores[0];
+            int minScore = scores[0];
+            int maxCount = 0;
+            int minCount = 0;
+            
+            List<int> result = new List<int>(2); 
+
+            for (int i = 0; i < scores.Count; i++)
+            {
+                if (scores[i] > maxScore)
+                {
+                    maxScore = scores[i];
+                    maxCount++;
+                }
+                else if (scores[i] < minScore)
+                {
+                    minScore = scores[i];
+                    minCount++;
+                }
+            }
+
+            result.Add(maxCount);
+            result.Add(minCount);
+            
+            return result;
+        }
+    }
+}
