6bb849f0c21b54d1b80b98217ce7413dd0514871
Jacob Renke <jacob.m.renke@outlook.com>
2022-01-30 22:07:26 -0500

    complete

---
+++ Plus Minus/Plus Minus/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Plus_Minus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static void plusMinus(List<int> arr)
+        {
+            double pos = 0;
+            double neg = 0;
+            double zero = 0;
+            double n = arr.Count;
+            double posRatio = 0;
+            double negRatio = 0;
+            double zeroRatio = 0;
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                if (arr[i] == 0)
+                {
+                    zero++;
+                }
+                else if (arr[i] > 0)
+                {
+                    pos++;
+                }
+                else if (arr[i] < 0)
+                {
+                    neg++;
+                }
+
+            }
+
+            posRatio = pos / n;
+            negRatio = neg / n;
+            zeroRatio = zero / n;
+
+            Console.WriteLine("{0:0.000000}\n{1:0.000000}\n{2:0.000000}", posRatio, negRatio, zeroRatio);
+        }
+    }
+}
