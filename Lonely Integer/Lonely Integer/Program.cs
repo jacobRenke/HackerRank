7487ef7a4ec023af521cb5e1cf3f6aefb4c4fdca
Jacob Renke <jacob.m.renke@outlook.com>
2022-02-03 12:36:24 -0500

    complete

--- Lonely Integer/Lonely Integer/Program.cs
+++ Lonely Integer/Lonely Integer/Program.cs
@@ -7,7 +7,7 @@ namespace Lonely_Integer
     {
         static void Main(string[] args)
         {
-            List<int> list = new List<int> { 15, 60, 74, 1, 94, 93, 28, 48, 70, 98, 45, 94, 42, 45, 48, 1, 72, 9, 24, 93, 41, 70, 60, 28, 11, 20, 72, 35, 11, 98, 31, 74, 31, 41, 9, 42, 20, 35, 24 };
+            List<int> list = new List<int> { 1, 1, 2 };
             Console.WriteLine(lonelyinteger(list));
         }
 
