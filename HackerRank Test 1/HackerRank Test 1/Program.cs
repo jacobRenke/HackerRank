14a2f6d7d6fc4a19421e4a270e1ba419c1c91ce6
Jacob Renke <jacob.m.renke@outlook.com>
2022-02-03 11:33:08 -0500

    complete

---
+++ HackerRank Test 1/HackerRank Test 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HackerRank_Test_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> arr = new List<int>(6) { 0, 1, 3, 4, 6, 5,};
+            Console.WriteLine(findMedian(arr));
+        }
+        public static int findMedian(List<int> arr)
+        {
+            int[] array = arr.ToArray();
+            Array.Sort(array);
+            int result = 0;
+
+            if ((array.Length / 2) == 0)
+            {
+                result = (array[7 / 2]) + 1;
+            }
+            else
+            {
+                result = (array[7 / 2]);
+            }
+            return result;
+        }
+
+        
+    }
+}
---
+++ Lonely Integer/Lonely Integer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lonely_Integer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
