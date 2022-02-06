495641b773a7416aa48c0fc297940489b1bc5c45
Jacob Renke <jacob.m.renke@outlook.com>
2022-02-05 22:44:03 -0500

    complete

---
+++ Counting Valleys/Counting Valleys/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Counting_Valleys
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(countingValleys(8, "UDDDUDUU"));
+        }
+
+        public static int countingValleys(int steps, string path)
+        {
+            int valleyCounter = 0;
+            int sealevel = 0;
+            bool flag = false;
+
+            char[] travel = path.ToCharArray();
+
+            while (!flag)
+            {
+                for (int i = 0; i < travel.Length - 1; i++)
+                {
+                    if (travel[i] == 'D' && sealevel == 0)
+                    {
+                        valleyCounter++;
+                        sealevel--;
+                    }
+                    else if (travel[i] == 'D')
+                    {
+                        sealevel--;
+                    }
+                    else if (travel[i] == 'U')
+                    {
+                        sealevel++;
+                    }
+                    else if (sealevel == 0)
+                    {
+                        break;
+                    }
+                }
+                flag = true;
+                
+            }
+            return valleyCounter;
+
+
+
+
+        }
+
+
+    }
+}
