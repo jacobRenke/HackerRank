45a737f45ddb35373675b2ffb9629a5986285c57
Jacob Renke <jacob.m.renke@outlook.com>
2022-02-09 16:50:29 -0500

    complete

---
+++ Week 2/Mars Exploration/Mars Exploration/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mars_Exploration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "SOSSOSSOS";
+            Console.WriteLine(marsExploration(s));
+        }
+
+        public static int marsExploration(string s) //return an integer for how many times a letter was changed from SOS
+        {
+            int counter = 0;
+            int length = s.Length / 3;
+            char[] ch = s.ToCharArray();
+            string comparisionString = String.Concat(Enumerable.Repeat("SOS", length));
+            char[] comparision = comparisionString.ToCharArray();
+
+            for (int i = 0; i < ch.Length; i++)
+            {
+                if (ch[i] != comparision[i])
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+    }
+}
